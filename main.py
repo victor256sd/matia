906e7e3efd786ad521b26a643f2cf99c6c503170
victor256sd <victor256@gmail.com>
2025-06-29 18:07:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -68,6 +68,42 @@ def generate_response(filename, openai_api_key, model, query_text):
         
     return messages, TMP_FILE_ID, TMP_VECTOR_STORE_ID, client, run, thread
 
+def generate_response_noassist(filename, openai_api_key, model, query_text):    
+    # Load document if file is uploaded
+    if filename is not None:
+        client = OpenAI(api_key=openai_api_key)
+        thread = client.beta.threads.create()
+
+        client.beta.threads.messages.create(
+            thread_id=thread.id, role="user", content=query_text
+        )
+        
+        file = client.files.create(
+            file=open(filename, "rb"),
+            purpose="user_data"
+        )
+
+        vector_store = client.vector_stores.create(
+            name="matia"
+        )
+        
+        TMP_VECTOR_STORE_ID = str(vector_store.id)
+        TMP_FILE_ID = str(file.id)
+                        
+        batch_add = client.vector_stores.file_batches.create(
+            vector_store_id=TMP_VECTOR_STORE_ID,
+            file_ids=[TMP_FILE_ID]
+        )
+
+        run = client.beta.threads.runs.create(
+            thread_id=thread.id
+        )
+
+        run = wait_on_run(client, run, thread)
+        messages = get_response(client, thread)
+        
+    return messages, TMP_FILE_ID, TMP_VECTOR_STORE_ID, client, run, thread
+
 def delete_vectors(client, TMP_FILE_ID, TMP_VECTOR_STORE_ID):
     # Delete the file and vector store
     deleted_vector_store_file = client.vector_stores.files.delete(
@@ -157,7 +193,7 @@ if doc_ex:
 
             if submit_doc_ex_form:                    
                 with st.spinner('Calculating...'):
-                    (response, TMP_FILE_ID, TMP_VECTOR_STORE_ID, client, run, thread) = generate_response("temp.txt", openai_api_key, model, query_doc_ex)
+                    (response, TMP_FILE_ID, TMP_VECTOR_STORE_ID, client, run, thread) = generate_response_noassist("temp.txt", openai_api_key, model, query_doc_ex)
             
                 st.write("*Matia is an AI-driven platform designed to review and analyze documents. The system continues to be refined. Users should review the original file and verify the summary for reliability and relevance.*")
                 # st.write("#### Summary")
