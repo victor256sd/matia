b18067bbb588bb96f5fd0f98c199afe184b885d5
victor256sd <victor256@gmail.com>
2025-07-19 23:43:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -463,6 +463,7 @@ if st.session_state.get('authentication_status'):
             # client3 = AsyncOpenAI(api_key=openai_api_key)
             # Query the aitam library vector store and include internet
             # serach results.
+            client = AsyncOpenAI(api_key=openai_api_key)
             with st.spinner('Calculating...'):
                 response3 = asyncio.run(generate_response_cmte(openai_api_key, VECTOR_STORE_ID, query))
             st.markdown("#### Response")
