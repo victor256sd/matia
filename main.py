22c84ad7fca2fe81387fff48e5f6b1e38cd64731
victor256sd <victor256@gmail.com>
2025-07-22 07:50:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -99,9 +99,9 @@ if st.session_state.get('authentication_status'):
             st.markdown("#### Token Usage")
             st.markdown(response2.usage)
 
-            cost = response2.usage.prompt_tokens * .1 / 10^6 + response2.usage.completion_tokens * .4 / 10^6
-            formatted_cost = "${:,.4f}".format(amount)
-            st.markdown(formatted_cost)
+            # cost = response2.usage.prompt_tokens * .1 / 10^6 + response2.usage.completion_tokens * .4 / 10^6
+            # formatted_cost = "${:,.4f}".format(amount)
+            # st.markdown(formatted_cost)
             
 elif st.session_state.get('authentication_status') is False:
     st.error('Username/password is incorrect')
